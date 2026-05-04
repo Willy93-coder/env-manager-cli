7e8ccd28816f52ef832056d997468440602a92a5
Willy93-coder <guillecg13@gmail.com>
2026-05-04 12:01:23 +0200

    feat: add init command to create projects in the database

--- cmd/init.go
+++ cmd/init.go
@@ -1,12 +1,23 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Create a new project in the database",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		project, err := database.CreateProject(args[0], nil)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Project '%s' created successfully\n", project.Name)
+
 		return nil
 	},
 }
