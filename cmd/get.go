f219d1271aa74a59f42c789ba8df7f2d2918d840
Willy93-coder <guillecg13@gmail.com>
2026-05-06 12:05:20 +0200

    feat: implement get commnand

--- cmd/get.go
+++ cmd/get.go
@@ -1,12 +1,28 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 var getCmd = &cobra.Command{
 	Use:   "get",
 	Short: "Get a variable by key",
 	Args:  cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		project, err := database.GetProjectByName(args[0])
+		if err != nil {
+			return err
+		}
+
+		env, err := database.GetEnvironmentByKey(project.ID, args[1])
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("%s=%s\n", env.Key, env.Value)
+
 		return nil
 	},
 }
