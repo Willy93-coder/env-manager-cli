8fceb1cb5adedd4fb2d0c8adb3f3046a8cd3f160
Willy93-coder <guillecg13@gmail.com>
2026-05-12 08:37:56 +0200

    feat: add list command to list all environments variables per project

--- cmd/list.go
+++ cmd/list.go
@@ -1,12 +1,30 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all environment variables",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		project, err := database.GetProjectByName(args[0])
+		if err != nil {
+			return err
+		}
+
+		environments, err := database.GetEnvironments(project.ID)
+		if err != nil {
+			return err
+		}
+
+		for _, env := range environments {
+			fmt.Printf("%s=%s\n", env.Key, env.Value)
+		}
+
 		return nil
 	},
 }
